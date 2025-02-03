ad67c42cb4609b7d3d03bd9e603cc4fd10012268
Manuel Proß <pro_manuel@yahoo.de>
2025-02-03 16:40:22 +0100

    feat: add type for data

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,25 @@
-export type Category = "Dimensions" | "Physical Attributes" | "Appearance" | "Composition" | "Power" | "Display" | "Hardware" | "Memory" | "Software" | "Camera" | "Connectivity" | "Features" | "Durability" | "Audio"
+export type Category =
+  | "Dimensions"
+  | "Physical Attributes"
+  | "Appearance"
+  | "Composition"
+  | "Power"
+  | "Display"
+  | "Hardware"
+  | "Memory"
+  | "Software"
+  | "Camera"
+  | "Connectivity"
+  | "Features"
+  | "Durability"
+  | "Audio";
 
 export type TechSpec = {
   id: number;
   label: string;
-  category: 
-}
+  category: Category;
+  note: string;
+  user_value: string;
+  system_value: string;
+  name: string;
+};
