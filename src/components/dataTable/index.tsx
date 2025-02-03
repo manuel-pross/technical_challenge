0516fc4babbef9fc5d5bc51e77c0a0f44d585702
Manuel Proß <pro_manuel@yahoo.de>
2025-02-03 16:28:22 +0100

    feat: add fetching, loading state and error handling

--- src/components/dataTable/index.tsx
+++ src/components/dataTable/index.tsx
@@ -1,19 +1,36 @@
+import { TechSpec } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
 function DataTable() {
-  //const {
-  //  data: techSpecs,
-  //  isLoading,
-  //  error,
-  //} = useQuery({
-  //  queryKey: ["techSpecs"],
-  //  refetchOnWindowFocus: false,
-  //  queryFn: async () => {
-  //    const res = await fetch("./src/assets/data.json");
-  //  },
-  //});
+  const {
+    data: techSpecs,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["techSpecs"],
+    refetchOnWindowFocus: false,
+    queryFn: async () => {
+      const res = await fetch("./src/assets/data.json");
 
-  return <div>hello from table</div>;
+      const techSpecs = (await res.json()) as TechSpec[];
+      console.log(techSpecs);
+      return techSpecs;
+    },
+  });
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+
+  if (!techSpecs) {
+    return <p>sth went wrong...</p>;
+  }
+
+  if (error) {
+    return <p>an error occurred</p>;
+  }
+
+  return <pre>{JSON.stringify(techSpecs)}</pre>;
 }
 
 export default DataTable;
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Category = "Dimensions" | "Physical Attributes" | "Appearance" | "Composition" | "Power" | "Display" | "Hardware" | "Memory" | "Software" | "Camera" | "Connectivity" | "Features" | "Durability" | "Audio"
+
+export type TechSpec = {
+  id: number;
+  label: string;
+  category: 
+}
