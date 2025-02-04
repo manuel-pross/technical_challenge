c532c89ac69abd6679759f30885c35b796e47c23
Manuel Proß <pro_manuel@yahoo.de>
2025-02-04 11:10:01 +0100

    refactor: set initial searchTerm store value to empty string

--- src/stores/searchTermStore.ts
+++ src/stores/searchTermStore.ts
@@ -1,13 +1,13 @@
 import { create } from "zustand";
 
 type SearchTermStore = {
-  searchTerm: string | null;
+  searchTerm: string;
   resetSearchTerm: () => void;
   updateSearchTerm: (newSearchTerm: string) => void;
 };
 
 export const useSearchTermStore = create<SearchTermStore>()((set) => ({
-  searchTerm: null,
+  searchTerm: "",
   resetSearchTerm: () => set(() => ({ searchTerm: null })),
   updateSearchTerm: (newSearchTerm: string) =>
     set(() => ({ searchTerm: newSearchTerm })),
